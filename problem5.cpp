496445ed149d6e4c9ef3c8ee78aca2a8e771b138
Javoxir Ismoilov <j.ismoilov@newuu.uz>
2024-09-28 13:57:12 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -7,7 +7,7 @@ int main() {
     int i;
 
     for(i = 1; i <= a; i++) {
-        if (i % 3 != 0 or i % 10 != 3) {
+        if (i % 3 != 0 and i % 10 != 3) {
             a++;
             continue;
         }
